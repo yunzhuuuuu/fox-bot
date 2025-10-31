5ee4ce23e7848d8ae3e8055b4521023a7bdc9579
Pepper Wang <pwang@olin.edu>
2025-10-31 09:06:31 -0400

    stuff

--- robot/pitch_finder.py
+++ robot/pitch_finder.py
@@ -4,13 +4,11 @@ import numpy as np
 
 # Load the audio as a waveform `y`
 # Store the sampling rate as `sr`
-y, sr = librosa.load(librosa.example('trumpet'))
+y, sr = librosa.load("440hz.wav")
 
 # Estimate fundamental frequency (f0) with probabilistic YIN
 f0, voiced_flag, voiced_probs = librosa.pyin(
-    y,
-    fmin=librosa.note_to_hz('C2'),
-    fmax=librosa.note_to_hz('C7')
+    y, fmin=librosa.note_to_hz("C2"), fmax=librosa.note_to_hz("C7")
 )
 
 # Replace unvoiced frames with NaN
@@ -19,3 +17,10 @@ f0 = np.where(voiced_flag, f0, np.nan)
 print(f0)
 # notes = librosa.hz_to_note(f0)
 # print(notes)
+
+f0 = f0[~np.isnan(f0)]
+
+print("\n")
+print(f0)
+
+print(librosa.hz_to_note(f0))
